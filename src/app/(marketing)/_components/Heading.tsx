e3a199fa6329b6067d8cb59e5b045838113d3e75
Kreylanc <devjanpun@gmail.com>
2023-10-16 10:33:58 +0100

    feat: check for user auth

--- src/app/(marketing)/_components/Heading.tsx
+++ src/app/(marketing)/_components/Heading.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import Spinner from "@/components/Spinner";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 function Heading() {
+  const { isLoading, isAuthenticated } = useConvexAuth();
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-4xl sm:text-6xl xl:text-7xl font-bold">
@@ -12,14 +17,33 @@ function Heading() {
       <p className="md:text-xl">
         Notion is the connected workspace where better, faster work happens.
       </p>
-      <Button size="lg">
-        Join Notion
-        <ArrowRight
-          aria-label="arrow right"
-          size={18}
-          className="ml-1.5 text-white"
-        />
-      </Button>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button size="lg" asChild>
+          <Link href="/documents">Enter Notion</Link>
+          <ArrowRight
+            aria-label="arrow right"
+            size={18}
+            className="ml-1.5 text-white"
+          />
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button size="lg">
+            Get Notion Free
+            <ArrowRight
+              aria-label="arrow right"
+              size={18}
+              className="ml-1.5 text-white"
+            />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 }
--- src/app/(marketing)/_components/Navbar.tsx
+++ src/app/(marketing)/_components/Navbar.tsx
@@ -5,28 +5,61 @@ import { Button } from "@/components/ui/button";
 import useScrollTop from "@/hooks/useScrollTop";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import Spinner from "@/components/Spinner";
 
 const Navbar = () => {
   const scrolled = useScrollTop();
+
+  // convex auth hook for checking user is logged in or out
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
-    <nav
+    <header
       className={cn(
-        "flex bg-background fixed w-full px-6 py-4 top-0 z-50 items-center dark:bg-neutral-900",
+        "flex bg-background fixed w-full px-6 py-4 top-0 z-50 items-center gap-3 dark:bg-neutral-900 transition",
         scrolled && "border-b shadow-sm"
       )}
     >
-      <Link href="/" className="font-bold">
+      <Link href="/" className="font-bold text-xl">
         Notion
       </Link>
-      <ul className="w-full flex justify-end gap-3 md:ml-auto">
-        <li>
-          <ModeToggle />
-        </li>
-        <li>
-          <Button>Login</Button>
-        </li>
-      </ul>
-    </nav>
+
+      <nav className="w-full flex items-center justify-end md:ml-auto">
+        <ul className="flex items-center gap-3">
+          {isLoading && (
+            <li>
+              <Spinner />
+            </li>
+          )}
+          {!isAuthenticated && !isLoading && (
+            <>
+              <li>
+                <SignInButton mode="modal">
+                  <Button variant="ghost">Log in</Button>
+                </SignInButton>
+              </li>
+              <li>
+                <SignInButton mode="modal">
+                  <Button>Get Notion Free</Button>
+                </SignInButton>
+              </li>
+            </>
+          )}
+          {isAuthenticated && !isLoading && (
+            <>
+              <li>
+                <Button variant="ghost">Enter Notion</Button>
+              </li>
+              <li>
+                <UserButton afterSignOutUrl="/" />
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+      <ModeToggle />
+    </header>
   );
 };
 

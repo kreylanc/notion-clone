77d599b741dbbc1db563672c718af53d6a4485fc
Kreylanc <devjanpun@gmail.com>
2023-10-18 13:13:00 +0100

    feat: create note from dashboard

--- src/app/(main)/(routes)/documents/page.tsx
+++ src/app/(main)/(routes)/documents/page.tsx
@@ -2,11 +2,27 @@
 
 import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
 import { PlusCircle } from "lucide-react";
 import Image from "next/image";
+import { api } from "../../../../../convex/_generated/api";
+import { toast } from "sonner";
 
 const DocumentPage = () => {
   const { user } = useUser();
+
+  const createDocument = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = createDocument({ title: "Untitled" });
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note.",
+    });
+  };
+
   return (
     <div className="h-full flex flex-col gap-2 items-center justify-center">
       <Image
@@ -27,7 +43,7 @@ const DocumentPage = () => {
         Welcome to{" "}
         <span className="text-blue-600">{user?.username}&apos;s</span> Notion
       </h1>
-      <Button className="mt-2">
+      <Button onClick={onCreate} className="mt-2">
         <PlusCircle className="mr-1.5" size={16} />
         Create a note
       </Button>

1464b1ce5161aa5ac5931261cffe53347ac4092b
Kreylanc <devjanpun@gmail.com>
2023-10-31 10:57:14 +0000

    state: zustand cover image hook

---
+++ src/hooks/useCoverImage.tsx
@@ -0,0 +1,12 @@
+import { create } from "zustand";
+
+type CoverImageProps = {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+};
+export const useCoverImage = create<CoverImageProps>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
